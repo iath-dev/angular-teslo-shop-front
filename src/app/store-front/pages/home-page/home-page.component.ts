d3ac6e097e3dd013376607cf314e4544d1534c4a
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-08-05 16:52:04 -0500

    feat: product basic page completed

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '@app/products/components/product-card/product-card.component';
 
 @Component({
-  selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  products = Array.from({ length: 12 });
+}
