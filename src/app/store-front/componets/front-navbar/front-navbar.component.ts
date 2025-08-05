48797265efe8ae0d006ac40262fa3b317228a7ff
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-08-05 15:59:18 -0500

    feat: navbar created

--- src/app/store-front/componets/front-navbar/front-navbar.component.ts
+++ src/app/store-front/componets/front-navbar/front-navbar.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface NavItem {
+  path: string;
+  title: string;
+}
+
+const routes: NavItem[] = [
+  {
+    path: '/gender/men',
+    title: 'Hombres',
+  },
+  {
+    path: '/gender/women',
+    title: 'Mujeres',
+  },
+  {
+    path: '/gender/kids',
+    title: 'Niños',
+  },
+];
 
 @Component({
   selector: 'front-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './front-navbar.component.html',
 })
-export class FrontNavbarComponent {}
+export class FrontNavbarComponent {
+  navRoutes = routes;
+}
